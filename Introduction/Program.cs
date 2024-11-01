6591eeb2688ded0be9c3d6f8a9645acfe492bd3b
Marin_vc47@top-academy.ru <Marin_vc47@top-academy.ru>
2024-11-01 17:15:23 +0500

    Insert Books and Authors DONE

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -6,87 +6,12 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 
-//namespace Introduction
-//{
-
-//	class Program
-//	{
-//		const string connectionString = @"
-//                Data Source=(localdb)\MSSQLLocalDB;
-//                Initial Catalog=Library;
-//                Integrated Security=True;
-//                Connect Timeout=30;Encrypt=False;
-//                TrustServerCertificate=False;
-//                ApplicationIntent=ReadWrite;
-//                MultiSubnetFailover=False";
-//		static SqlConnection connection;
-//		static Program()
-//		{
-//			connection = new SqlConnection(connectionString);
-//		}
-//		static void Main(string[] args)
-//		{
-//			SelectAuthors();
-
-//			string cmd = " SELECT title, first_name, last_name FROM Books, Authors WHERE author = author_id";
-//			SqlCommand command = new SqlCommand( cmd,connection);
-//			connection.Open();
-//			SqlDataReader reader = command.ExecuteReader();
-//			if (reader.HasRows)
-//			{
-//				int padding = 32;
-//				for (int i = 0; i < reader.FieldCount; i++)
-//					Console.Write(reader.GetName(i).PadRight(padding));
-//					Console.WriteLine();
-//				while (reader.Read())
-//				{
-//					for (int i = 0; i < reader.FieldCount; i++)
-//					{ 
-//						Console.Write(reader.GetValue(i).ToString().PadRight(padding));
-//					}
-//						Console.WriteLine("\n---------------------------------------------------\n");
-//				}
-//				Console.WriteLine();
-//			}
-//			connection.Close();
-//		}
-//		static void SelectAuthors()
-//		{
-//			SqlCommand command = new SqlCommand();
-//			command.Connection = connection;
-//			command.CommandText = "SELECT * FROM Authors";
-
-//			connection.Open();
-//			SqlDataReader reader = command.ExecuteReader();
-//			const int padding = 32;
-//			if (reader.HasRows)
-//			{
-//				for (int i = 0; i < reader.FieldCount; i++)
-//					Console.Write(reader.GetName(i).PadRight(padding));
-//				Console.WriteLine();
-
-//				while (reader.Read())
-//				{
-
-//					//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
-//					for (int i = 0; i < reader.FieldCount; i++)
-//						Console.Write(reader.GetValue(i).ToString().PadRight(padding));
-//					Console.WriteLine();
-//				}
-
-//			}
-//			connection.Close();
-//			Console.WriteLine("Done");
-//		}
-//	}
-//}
-
-
 namespace Introduction
 {
-    class Program
-    {
-        const string connectionString = @"
+
+	class Program
+	{
+		const string connectionString = @"
                 Data Source=(localdb)\MSSQLLocalDB;
                 Initial Catalog=Library;
                 Integrated Security=True;
@@ -94,53 +19,140 @@ namespace Introduction
                 TrustServerCertificate=False;
                 ApplicationIntent=ReadWrite;
                 MultiSubnetFailover=False";
+		static SqlConnection connection;
+		static Program()
+		{
+			connection = new SqlConnection(connectionString);
+		}
+		static void Main(string[] args)
+		{
+			//SelectAuthors();
+			//SelectBooks();
+			//Select ("book_id,title,last_name, first_name", "Books,Authors", "author=author_id");
 
-        static SqlConnection connection;
+			//Console.Write("Введите имя: ");
+			//string first_name = Console.ReadLine();
+			//Console.Write("Введите фамилию: ");
+			//string last_name = Console.ReadLine();
+			//InsertAuthor(first_name,last_name);
 
-        static Program()
-        {
-            connection = new SqlConnection(connectionString);
-        }
+			//Console.WriteLine(GetAuthorId("James", "Cameron"));
+			//SelectAuthors();
 
-        static void Main(string[] args)
-        {
-            Select("Authors", "author_id, first_name, last_name");
-            Select("Books", "title, author");
-            Select("Books, Authors", "title, first_name, last_name");
-        }
+			Console.Write("Введите название книги: ");
+			string title = Console.ReadLine();
+			Console.Write("Введите имя автора: ");
+			string first_name = Console.ReadLine();
+			Console.Write("Введите фамилию автора: ");
+			string last_name = Console.ReadLine();
+			InsertBook(title, first_name, last_name);
+			SelectBooks();
+		}
+		static void InsertBook(string title,string first_name,string last_name)
+		{
+			int author = GetAuthorId(first_name,last_name);
+			string cmd = $"INSERT Books(title,author) VALUES ('{title}', '{author}')";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			command.ExecuteNonQuery();
+			connection.Close();
+		}
+		static int GetAuthorId(string first_name, string last_name)
+		{
+			string cmd = $"(SELECT author_id FROM Authors WHERE first_name = N'{first_name}' AND last_name = N'{last_name}')";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			int author_id = Convert.ToInt32(command.ExecuteScalar());
+			connection.Close();
+			return author_id;
+		}
+		static void InsertAuthor(string first_name, string last_name)
+		{
+			string cmd = $"INSERT  Authors(first_name,last_name)VALUES ('{first_name}','{last_name}')";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			command.ExecuteNonQuery();
+			connection.Close();
+		}
+		static void Select(string columns, string tables, string condition)
+		{ 
+			string cmd = $"Select {columns} FROM {tables} WHERE {condition}";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			if (reader.HasRows)
+			{
+				int padding = 28;
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					Console.Write(reader.GetName(i).PadRight(padding));
+				}
+				Console.WriteLine("\n------------------------------------------------------------------------------------\n");
+				while (reader.Read())
+				{
+					for (int i = 0; i < reader.FieldCount; i++)
+					{
+						Console.Write(reader[i].ToString().PadRight(padding));
+					}
+					Console.WriteLine();
+				}
+			}
+			reader.Close();
+			connection.Close();
+		}
 
-        static void Select(string tables, string fields)
-        {
-            string cmd = $"SELECT {fields} FROM {tables}";
+		static void SelectBooks()
+		{ string cmd = " SELECT title, first_name, last_name FROM Books, Authors WHERE author = author_id";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			if (reader.HasRows)
+			{
+				int padding = 32;
+				for (int i = 0; i < reader.FieldCount; i++)
+					Console.Write(reader.GetName(i).PadRight(padding));
+				Console.WriteLine();
+				while (reader.Read())
+				{
+					for (int i = 0; i < reader.FieldCount; i++)
+					{
+						Console.Write(reader.GetValue(i).ToString().PadRight(padding));
+					}
+					Console.WriteLine("\n---------------------------------------------------\n");
+				}
+				Console.WriteLine();
+			}
+			connection.Close();
+		}
+		static void SelectAuthors()
+		{
+			SqlCommand command = new SqlCommand();
+			command.Connection = connection;
+			command.CommandText = "SELECT * FROM Authors";
 
-            SqlCommand command = new SqlCommand(cmd, connection);
-            connection.Open();
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			const int padding = 32;
+			if (reader.HasRows)
+			{
+				for (int i = 0; i < reader.FieldCount; i++)
+					Console.Write(reader.GetName(i).PadRight(padding));
+				Console.WriteLine();
 
-            SqlDataReader reader = command.ExecuteReader();
-            const int padding = 32;
-            if (reader.HasRows)
-            {
-                // Вывод заголовков
-                for (int i = 0; i < reader.FieldCount; i++)
-                    Console.Write(reader.GetName(i).PadRight(padding));
-                Console.WriteLine();
+				while (reader.Read())
+				{
 
-                // Вывод данных
-                while (reader.Read())
-                {
-                    for (int i = 0; i < reader.FieldCount; i++)
-                        Console.Write(reader.GetValue(i).ToString().PadRight(padding));
-                    Console.WriteLine();
-                }
-                Console.WriteLine("\n---------------------------------------------------\n");
-            }
-            else
-            {
-                Console.WriteLine("Нет данных для отображения.");
-            }
+					//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+					for (int i = 0; i < reader.FieldCount; i++)
+						Console.Write(reader.GetValue(i).ToString().PadRight(padding));
+					Console.WriteLine();
+				}
 
-            connection.Close();
-        }
-    }
+			}
+			connection.Close();
+			Console.WriteLine("Done");
+		}
+	}
 }
+
 
