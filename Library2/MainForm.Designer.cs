5693ffacff1efa54ff9beb5388f4a0e651311ea5
Marin_vc47@top-academy.ru <Marin_vc47@top-academy.ru>
2024-11-08 16:20:25 +0500

    ClassWork Done

--- Library2/MainForm.Designer.cs
+++ Library2/MainForm.Designer.cs
@@ -34,6 +34,7 @@ namespace Library2
 			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.buttonExecute = new System.Windows.Forms.Button();
 			this.dataGridView = new System.Windows.Forms.DataGridView();
+			this.comboBoxTables = new System.Windows.Forms.ComboBox();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -68,16 +69,27 @@ namespace Library2
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView.Location = new System.Drawing.Point(18, 41);
+			this.dataGridView.Location = new System.Drawing.Point(18, 65);
 			this.dataGridView.Name = "dataGridView";
-			this.dataGridView.Size = new System.Drawing.Size(481, 211);
+			this.dataGridView.Size = new System.Drawing.Size(481, 187);
 			this.dataGridView.TabIndex = 3;
+			// 
+			// comboBoxTables
+			// 
+			this.comboBoxTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxTables.FormattingEnabled = true;
+			this.comboBoxTables.Location = new System.Drawing.Point(18, 38);
+			this.comboBoxTables.Name = "comboBoxTables";
+			this.comboBoxTables.Size = new System.Drawing.Size(392, 21);
+			this.comboBoxTables.TabIndex = 5;
+			this.comboBoxTables.SelectedIndexChanged += new System.EventHandler(this.comboBoxTables_SelectedIndexChanged);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(505, 264);
+			this.Controls.Add(this.comboBoxTables);
 			this.Controls.Add(this.dataGridView);
 			this.Controls.Add(this.buttonExecute);
 			this.Controls.Add(this.textBoxQuery);
@@ -95,6 +107,7 @@ namespace Library2
 		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
 		private System.Windows.Forms.Button buttonExecute;
 		private System.Windows.Forms.DataGridView dataGridView;
+		private System.Windows.Forms.ComboBox comboBoxTables;
 	}
 }
 
--- Library2/MainForm.cs
+++ Library2/MainForm.cs
@@ -12,17 +12,35 @@ using System.Configuration;
 
 namespace Library2
 {
-	
+
 	public partial class MainForm : Form
 	{
 		//readonly string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString_2_Library_PD_311"].ConnectionString;
 		readonly string connectionString = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
-			SqlConnection connection;
+		SqlConnection connection;
 		public MainForm()
 		{
 			connection = new SqlConnection(connectionString);
 			InitializeComponent();
+			LoadTablesToComboBox();
 		}
+		void LoadTablesToComboBox()
+		{
+			string cmd = "SELECT table_name FROM information_schema.tables;";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			if (reader.HasRows)
+			{
+				while (reader.Read())
+				{
+					comboBoxTables.Items.Add(reader[0]);
+				}
+			}
+			reader.Close();
+			connection.Close();
+		}
+
 
 		private void buttonEqu_Click(object sender, EventArgs e)
 		{
@@ -41,13 +59,46 @@ namespace Library2
 				for (int i = 0; i < reader.FieldCount; i++)
 				{
 					row[i] = reader[i];
-					
+
 				}
 				table.Rows.Add(row);
 			}
 			reader.Close();
 			connection.Close();
 			dataGridView.DataSource = table;
+		}
+
+		private void comboBoxTables_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			dataGridView.DataSource = null;
+			SelectAllFromTable((sender as ComboBox).Text);
+		}
+		void SelectAllFromTable(string table_name)
+		{
+			string cmd = $"SELECT * FROM [{table_name}]";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			if (reader.HasRows)
+			{
+				DataTable table = new DataTable();
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					table.Columns.Add(reader.GetName(i));
+				}
+				while (reader.Read())
+				{
+					DataRow row = table.NewRow();
+					for (int i = 0; i < reader.FieldCount; i++)
+					{
+						row[i] = reader[i];
+					}
+					table.Rows.Add(row);
+					dataGridView.DataSource = table;
+				}
+			}
+			reader.Close();
+			connection.Close();
 		}
 	}
 }
